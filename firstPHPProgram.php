5c37e3f2484412b0ab825b27ce27c6bc917092c3
vHast <fedvhast@gmail.com>
2022-04-21 14:01:01 +0300

    First commit

---
+++ firstPHPProgram.php
@@ -0,0 +1,7 @@
+<?php //test1.php
+  $username = "Fred Smith"; // We create a variable named $username
+  echo $username; // We print $username
+  echo "<br>"; // We print <br>
+  $current_user = $username; // We create a new variable and assign the value of $username to $current_user
+  echo $current_user; // We print $current_user
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  // Comment
+
+  /* Multi-line
+    Comment */
+
+  echo "Hello world";   //Pritns Hello World
+
+  // In PHP you must place a $ in front of all variables
+
+  $mycounter = 1;
+  $mystring = "Hello";
+  $myarray = array("One", "Two", "Three");
+
+  // In order to invoke a variable
+
+  echo $mystring;
+
+  // In order to invoke an element of an array
+
+  echo $myarray[1]; //This will display Two, since it's ZERO INDEX BASED
+
+  // We can also assign the variable to a different one
+
+  $current_user = $username;
+
+?>
